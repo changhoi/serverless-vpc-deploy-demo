ff57c82c39004bdcfc38ab3db369643eb323cbbc
changhoi <0417kch@naver.com>
2020-03-05 00:20:47 +0900

    Resource 연결한 상태

--- handler.ts
+++ handler.ts
@@ -1,12 +1,31 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
-import 'source-map-support/register';
+import { APIGatewayProxyHandler } from "aws-lambda";
+import { S3, config, DynamoDB } from "aws-sdk";
+import "source-map-support/register";
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
+config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY
+});
+
+export const hello: APIGatewayProxyHandler = async (_event, _context) => {
+  const s3 = new S3();
+  const dynamodb = new DynamoDB();
+  const s3Data = await s3
+    .listObjectsV2({ Bucket: "vpc-demo-bucket" })
+    .promise();
+  const { Items: dynamoData } = await dynamodb
+    .scan({ TableName: "vpc-demo-table" })
+    .promise();
+
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
+    body: JSON.stringify(
+      {
+        s3Data,
+        dynamoData
+      },
+      null,
+      2
+    )
   };
-}
+};
